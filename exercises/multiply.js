ebd9e4f43854a71444f0705173a5e26591eefd7e
Steven AD <s.dailliez@gmail.com>
2018-09-14 15:40:28 +0200

    Solved multiply.js

--- exercises/multiply.js
+++ exercises/multiply.js
@@ -11,15 +11,26 @@
 // Your code :
 
 function multiply (x, y){
-	if (y > 0){
-	for (let i = 0; i < y - 1; i++) {
-		x += x;
+	let resultat = 0;
+	if (x === 0 || y === 0)
+		return 0;
+	
+	if ((y > 0 && x > 0) || (y < 0 && x < 0)){
+		y = Math.abs(y);
+		x = Math.abs(x);
+		for (let i = 0; i < y; i++){
+			resultat += x;
+		}
+		return resultat;
+	} else{
+		if ((y > 0 && x < 0) || (y < 0 && x > 0)){
+			y = Math.abs(y);
+			x = Math.abs(x);
+			for (let i = 0; i < y; i++) {
+				resultat += x;
+			} return -resultat;
+		}
 	}
-	return x;
-	} else for (let i = 0; i > y - 1; i++) {
-		x += x;
-	}
-	return x;
 }
 
 //* Begin of tests
